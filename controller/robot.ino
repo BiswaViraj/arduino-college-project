a262ed25a3464f854c4ba608163e89ae7810e47c
Faizan Ahmad <faizan.nasro@gmail.com>
2019-12-10 10:11:26 +0530

    initial layout for final controller

---
+++ controller/robot.ino
@@ -0,0 +1,21 @@
+//This Arduino program drives a water dispenser robot.
+
+//Header inclusions
+
+//Global vars
+
+//Pin definitions
+
+void setup(){
+
+    //Define input pins
+
+    //Define output pins
+
+    //Initialize devices
+}
+
+void loop(){
+    
+
+}

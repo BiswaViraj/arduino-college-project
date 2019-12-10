090a551a175d1696b4e9ccec84a79da481ecbeef
Faizan Ahmad <faizan.nasro@gmail.com>
2019-12-10 12:15:16 +0530

    Updated with motor controller

--- controller/robot.ino
+++ controller/robot.ino
@@ -2,6 +2,7 @@
 
 //Header inclusions
 #include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
 
 //Global vars
 //LCD bytes
@@ -21,17 +22,41 @@ byte b121[] = {B00000,B00000,B00000,B11111,B00000,B00000,B00000,B00000};
 byte b131[] = {B00000,B00000,B00000,B11111,B00000,B00000,B00000,B00000};
 byte b141[] = {B00001,B00010,B00010,B00100,B00110,B01010,B01001,B10001};
 
+//Motor vars
+SoftwareSerial Blue(2,3);
+long int data;
+long int command1 = 92;// forward
+long int command2 = 79; // left
+long int command3 = 71; //  right
+long int command4 = 91; //  Reverse
+long int command5 = 10; // stop
+char state = 0;
+
 //Pin definitions
 int backlight = 13;
+//Motor pins
+int ena = 5;
+int enb = 6;
+int in1 = 8;
+int in2 = 9;
+int in3 = 10;
+int in4 = 11;
 
 void setup(){
     
     Serial.begin(9600);
+    Blue.begin(9600);
     
     //Define input pins
 
     //Define output pins
     pinMode(backLight, OUTPUT);
+    pinMode(ena, OUTPUT);
+    pinMode(enb, OUTPUT);
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
 
 
     //Initialize devices
@@ -39,11 +64,60 @@ void setup(){
     lcd.begin(16,2);
     lcd.clear();
 
+    analogWrite(ena, 255);
+    analogWrite(enb, 255);
+    delay(100);
 }
 
 void loop(){
 
 
+}
+
+void movement(int move){
+    if (move == command1) // Forward
+    {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    //delay(100);
+    }
+        
+    if(move == command2) // left
+    {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    delay(100);
+    }
+    
+    if(move == command3) // Right
+    {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+    delay(100);
+    }
+        
+    if(move == command4) // Reverse
+    {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    delay(100);
+    }
+    if(move == command5) // Stop
+    {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+    delay(100);
+    }
 }
 
 void smile(){
@@ -105,5 +179,4 @@ void smile(){
     /*lcd.createChar(13, b141);
     lcd.setCursor(14, 1);  
     lcd.write(byte(13));*/
-
 }
